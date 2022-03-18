aa0ce0b5242b0af5c43471a629cec4ccf7eca3c4
Michael Goll <se21m003@technikum-wien.at>
2022-03-18 09:44:09 +0100

    Add project files.

---
+++ SFR_ReactiveSystems.TransactionService/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SFR_ReactiveSystems.TransactionService;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Startup>();
+    })
+    .Build()
+    .RunAsync();
